966dadefb7caa67e195a2bdf54956352ed754e7d
qasim98 <35306677+qasim98@users.noreply.github.com>
2019-11-06 18:55:52 -0500

    Add files via upload

--- assignment2/partA/a2.sql
+++ assignment2/partA/a2.sql
@@ -1,4 +1,4 @@
-SET search_path TO A2;
+﻿SET search_path TO A2;
 -- Add below your SQL statements. 
 -- For each of the queries below, your final statement should populate the respective answer table (queryX) with the correct tuples. It should look something like:
 -- INSERT INTO queryX (SELECT … <complete your SQL query here> …)
@@ -8,13 +8,44 @@ SET search_path TO A2;
 -- Good Luck!
 
 --Query 1 statements
-INSERT INTO query1
+INSERT INTO query1 (
+	SELECT pname, cname, tname FROM champion, tournament, country, player
+	WHERE champion.tid = tournament.tid AND tournament.cid = country.cid AND country.cid = player.cid AND champion.pid = player.pid
+	ORDER BY pname ASC
+);
 
 --Query 2 statements
-INSERT INTO query2
+INSERT INTO query2 (
+	CREATE VIEW sumCaps AS 
+		SELECT tname, sum(capacity) AS sumCapacity
+		FROM tournament, court
+		WHERE tournament.tid = court.tid
+		GROUP By tname
+		ORDER BY tname ASC;
 
+	SELECT tname, max(sumCapacity) AS totalCapacity
+	FROM sumCaps;
+
+	DROP VIEW IF EXISTS sumCaps;
+)
 --Query 3 statements
-INSERT INTO query3
+INSERT INTO query3 (
+	CREATE VIEW winnerEvents AS
+		-- blah
+		
+	CREATE VIEW loserEvents AS
+		-- blah
+	
+	CREATE VIEW p1p2Events AS
+		-- blah
+		
+	SELECT -- blah
+	
+	
+	DROP VIEW IF EXISTS winnerEvents;
+	DROP VIEW IF EXISTS loserEvents;
+	DROP VIEW IF EXISTS p1p2Events;
+)
 
 --Query 4 statements
 INSERT INTO query4
