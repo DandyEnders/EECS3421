ab13db86c1738316e6fe03af44869db2745837ef
DandyEnders <howden2@gmail.com>
2019-11-08 13:08:00 -0500

    Attempted partB

--- assignment2/partB/Assignment2.java
+++ assignment2/partB/Assignment2.java
@@ -20,11 +20,47 @@ public class Assignment2 {
   
     //Using the input parameters, establish a connection to be used for this session. Returns true if connection is sucessful
     public boolean connectDB(String URL, String username, String password){
+        try {
+		
+ 			// Load JDBC driver
+			Class.forName("org.postgresql.Driver");
+ 
+		} catch (ClassNotFoundException e) {
+ 
+			//System.out.println("Where is your PostgreSQL JDBC Driver? Include in your library path!");
+			//e.printStackTrace();
+			return false;
+ 
+		}
+
+        System.out.println("PostgreSQL JDBC Driver Registered!");
+
+        try {
+			
+ 			//Make the connection to the database, ****** but replace <dbname>, <username>, <password> with your credentials ******
+			//System.out.println("*** Please make sure to replace 'dbname', 'username' and 'password' with your credentials in the jdbc connection string!!!");
+			connection = DriverManager.getConnection(URL + username, username, password);
+ 
+		} catch (SQLException e) {
+ 
+			//System.out.println("Connection Failed! Check output console");
+			//e.printStackTrace();
+			return false;
+ 
+		}
+
+        if (connection == null) {
+            // System.out.println("Failed to make connection!");
+            return false;
+        }
+ 
+        
         return false;
     }
   
     //Closes the connection. Returns true if closure was sucessful
     public boolean disconnectDB(){
+        connection.close();
         return false;    
     }
     
